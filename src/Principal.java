168fddb073d722eb7718f14e6ea65c32bb75e145
miguel <miguelpsantos2001@gmail.com>
2021-03-04 18:14:39 +0000

    2ª versão do Relógio_r1
    Foram incluídas as validações e a consideração dos intervalos rotativos dos segundos, minutos e horas.

--- src/Principal.java
+++ src/Principal.java
@@ -3,50 +3,116 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		int horas=10, minutos=25, segundos=36;
+		int horas=10, minutos=59, segundos=36;
 		
 		System.out.printf("%d:%d:%d %n", horas, minutos, segundos);
 		
 		horas = avancarHoras(horas);
-		minutos = avancarMinutos(minutos);
-		segundos = avancarSegundos(segundos);
 		
+		System.out.println("Avançar Horas ");
 		System.out.printf("%d:%d:%d %n", horas, minutos, segundos);
 		
-		horas = recuarHoras( recuarHoras(horas) );
-		minutos = recuarMinutos( recuarMinutos(minutos) );
-		segundos = recuarSegundos( recuarSegundos(segundos) );
+		int[] minHor = avancarMinutos(minutos, horas);
+		horas = minHor[1];
+		minutos = minHor[0];
 		
-		System.out.printf("%d:%d:%d %n", horas, minutos, segundos);
-
+		System.out.println("Avançar Minutos ");
+		System.out.printf("%d:%2d:%d %n", horas, minutos, segundos);
+		
+		
+		int[] segMinHor = avancarSegundos(segundos, minutos, horas);
+		horas = segMinHor[2];
+		minutos = segMinHor[1];
+		segundos = segMinHor[0];
+		
+		System.out.println("Avançar Segundos ");
+		System.out.printf("%d:%2d:%d %n", horas, minutos, segundos);
+		
 	}
 	
 	
 	
 	public static int avancarHoras( int horas ) {
-		return horas +  1;
+		
+		if( horas == 23 )
+			horas = 0;
+		else
+			horas = horas + 1;
+		
+		return horas;
 	}
 	
 	public static int recuarHoras( int horas ) {
-		return horas -  1;
+		
+		if( horas == 0 )
+			horas = 23;
+		else
+			horas = horas - 1;
+		
+		return horas;
 	}
 	
-	public static int avancarMinutos( int minutos ) {
-		return minutos +  1;
+	public static int[] avancarMinutos( int minutos, int horas ) {
+		
+		if( minutos == 59 ) {
+			minutos = 0;
+			horas = avancarHoras(horas);
+		}
+		else
+			minutos = minutos + 1;
+		
+		int[] minutosHoras = { minutos, horas };
+		
+		return minutosHoras;
 	}
 	
-	public static int recuarMinutos( int minutos ) {
-		return minutos -  1;
+	public static int[] recuarMinutos( int minutos, int horas ) {
+		
+		if( minutos == 0 ) {
+			minutos = 59;
+			horas = recuarHoras(horas);
+		}
+		else
+			minutos = minutos - 1;
+		
+		int[] minutosHoras = { minutos, horas };
+		
+		return minutosHoras;
+		
 	}
 	
-	public static int avancarSegundos( int segundos ) {
-		return segundos +  1;
+	public static int[] avancarSegundos( int segundos, int minutos, int horas ) {
+		
+		if( segundos == 59 ) {
+			segundos = 0;
+			int[] minutosHoras = avancarMinutos(minutos, horas);
+			minutos = minutosHoras[0];
+			horas = minutosHoras[1];
+		}
+		else
+			segundos = segundos + 1;
+		
+		int[] segundosMinutosHoras = { segundos, minutos, horas };
+		
+		return segundosMinutosHoras;
+		
 	}
 	
-	public static int recuarSegundos( int segundos ) {
-		return segundos -  1;
+	public static int[] recuarSegundos( int segundos, int minutos, int horas ) {
+		
+		if( segundos == 0 ) {
+			segundos = 59;
+			int[] minutosHoras = recuarMinutos(minutos, horas);
+			minutos = minutosHoras[0];
+			horas = minutosHoras[1];
+		}
+		else
+			segundos = segundos - 1;
+		
+		int[] segundosMinutosHoras = { segundos, minutos, horas };
+		
+		return segundosMinutosHoras;
 	}
 	
 	
-
 }
